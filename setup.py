8a04e5ebe70eea5d7f82accb95389f4a6a57f3a8
ryancoxrbc <102892449+ryancoxrbc@users.noreply.github.com>
2025-02-26 08:01:59 +0200

    Fixed mistake in url in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="Object-oriented wrapper for CoolProp thermodynamic properties",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ryancoxrbc/coolprop-oop",
+    url="https://github.com/ryancoxrbc/coolprop_oop",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
